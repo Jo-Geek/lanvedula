e268fb7d01a1c54f9013312a92d326fe61f8e6fb
Jones Joseph <jonesvinotjoseph@hotmail.com>
2019-08-06 22:42:04 +0530

    Initial layout and colors

---
+++ assets/js/lavendula.core.js
@@ -0,0 +1,12 @@
+window.lavendula = {
+  tooltip: () => {
+    $('[data-toggle=tooltip]').tooltip();
+  },
+  init: function (){
+    this.tooltip();
+  }
+}
+
+// lavendula.init = function () {
+//   lavendula.tooltip();
+// }
