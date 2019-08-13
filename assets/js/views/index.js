5262f2aa64f2982463a3d53a0e719daa4685906f
Jones Joseph <jonesvinotjoseph@hotmail.com>
2019-08-13 16:27:11 +0530

    Dashboard finalize

--- assets/js/views/index.js
+++ assets/js/views/index.js
@@ -306,10 +306,177 @@ var _chartAdditionals = function () {
   });
 }
 
+var _chartExpenseTrend = function () {
+  let ctx = document.getElementById('chartExpenseTrend').getContext('2d');
+
+  let gradientFill = ctx.createLinearGradient(0, 0, 0, 200);
+  gradientFill.addColorStop(0, "rgba(211,170,249, .6)");
+  gradientFill.addColorStop(1, "rgba(211,170,249, 0)");
+
+  window.chartExpenseTrend = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+      datasets: [{
+        label: 'Expense',
+        backgroundColor: gradientFill,
+        borderColor: "rgb(185,132,244)",
+        borderWidth: 1,
+        data: [
+
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+
+        ],
+        fill: true
+      }
+
+      ]
+    },
+    options: {
+      elements: {
+        point: {
+          radius: 2
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: true
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: 'rgba(150, 150, 150, 0.5)'
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: 'rgba(0, 0, 0, 0)'
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: ''
+      },
+      legend: {
+        display: false,
+        position: "bottom"
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: true
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+}
+
+var _chartSubscriberTrend = function () {
+  let ctx = document.getElementById('chartSubscriberTrend').getContext('2d');
+
+  let gradientFill = ctx.createLinearGradient(0, 0, 0, 200);
+  gradientFill.addColorStop(0, "rgba(132,223,187, .6)");
+  gradientFill.addColorStop(1, "rgba(132,223,187, 0)");
+
+  window.chartSubscriberTrend = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+      datasets: [{
+        label: 'Subscribers',
+        backgroundColor: gradientFill,
+        borderColor: "rgb(90,205,156)",
+        borderWidth: 1,
+        data: [
+
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+
+        ],
+        fill: true
+      },
+      {
+        label: 'Additionals',
+        backgroundColor: "rgba(226,206,85,.25)",
+        borderColor: "rgb(226,206,85)",
+        borderWidth: 1,
+        data: [
+
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+
+        ],
+        fill: true
+      }
+      ]
+    },
+    options: {
+      elements: {
+        point: {
+          radius: 2
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: true
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: 'rgba(0, 0, 0, 0)'
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: 'rgba(0, 0, 0, 0)'
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: ''
+      },
+      legend: {
+        display: true,
+        position: "top"
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: true
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+}
+
 $(document).ready(function () {
   _chartBillperHead();
   _chartExpenses();
   _chartSubscribers();
   _chartAdditionals();
-
+  _chartExpenseTrend();
+  _chartSubscriberTrend();
 })
